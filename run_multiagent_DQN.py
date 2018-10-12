4bb4a0317eb494982908ae00fb28edfba16c0e9a
shilx001 <shilx001@gmail.com>
2018-10-12 11:15:21 +1100

    Core code for Multiple Particle Environment

--- run_multiagent_DQN.py
+++ run_multiagent_DQN.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 from DQN_Agent import DeepQNetwork
 from particle_env import *
 
@@ -24,8 +25,8 @@ for episode in range(1000):
                 action.append(agent_action)
         next_state, reward, done = env.step(action)
         cum_reward += np.sum(np.array(reward))
-        if step==200-1:
-            print('Episode ',episode,'finished at reward ',cum_reward)
+        if step == 200 - 1:
+            print('Episode ', episode, 'finished at reward ', cum_reward)
         for i in range(3):  # store transitions and learn
             agent_observation = state[i]
             agent_observation_ = next_state[i]
@@ -36,11 +37,13 @@ for episode in range(1000):
                 agent_status[i] = True
                 continue
             # 否则存储当前，并进行学习
-            agents[i].store_transition(agent_observation, agent_action, agent_reward,agent_observation_,int(agent_done))
+            agents[i].store_transition(agent_observation, agent_action, agent_reward, agent_observation_,
+                                       int(agent_done))
             agents[i].learn()
-        state=next_state
+        state = next_state
     total_reward.append(cum_reward)
 
+pickle.dump(total_reward, open('Independent DQN', 'wb'))
 plt.plot(np.array(total_reward) / 200)
 plt.xlabel('Episode')
 plt.ylabel('Average reward')
